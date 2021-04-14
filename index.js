6f2c757b691d3745038ab9a14eaa672e7ca5669f
Moween <moween1998@gmail.com>
2021-04-14 10:53:56 +0100

    added codes.

--- index.js
+++ index.js
@@ -1,55 +1,104 @@
+const rockbtn = document.querySelector('#rock')
+const paperbtn = document.querySelector('#paper')
+const scissorsbtn = document.querySelector('#scissors')
+const result = document.querySelector('#result');
+const refreshBtn = document.getElementById('refresh')
+
+
 //Array to pick item from.
 const myArray = ["rock", "paper", "scissors"];
 
-// Computer
+//result array
+const winningArray = [
+  ['rock', 'scissors'],
+  ['scissors', 'paper'],
+  ['paper', 'rock']
+]
+
+//variable to increment scores 
+let playerScore = 0;
+let computerScore = 0;
+
+
+
+
+// Computer Play Function
 function computerPlay() {
   return myArray[Math.floor(Math.random() * myArray.length)];  
 }
 
-// player
-const playerPlay = () => {
-  let playerSelection = prompt('Pick one:\nRock, Paper, Scissors').toLowerCase();
-  while(!myArray.includes(playerSelection)) {
-    alert('Invalid input, try again!');
-    playerSelection = prompt('Pick one:\nRock, Paper, Scissors').toLowerCase();
+
+
+function playRound(e) {  
+  e.preventDefault();
+  e.stopPropagation(); 
+  
+  
+  //Computer Plays
+  let computerSelection = computerPlay();
+  console.log("Computer Selection:", computerSelection);
+
+  //Player Plays
+  playerSelection = e.target.id;
+  console.log(playerSelection);  
+  
+  //Checking for winner
+  if(playerSelection !== computerSelection) {
+    let playerWin = false;
+    for (let i = 0; i < winningArray.length; i++) {
+      if(JSON.stringify([playerSelection, computerSelection]) === JSON.stringify(winningArray[i])) {
+        playerScore += 10;
+        playerWin = true;
+        break;
+      }      
+    }
+    
+    if(!playerWin) {
+      computerScore += 10;
+    }
+    
   }
-  return playerSelection ;
-}
-
-
-//Round one
-function playRound(computerSelection, playerSelection) {
-  //variable to increment scores
-  let playerScore = 0;
-  let computerScore = 0;
-  let computerTurn;
-  let playerTurn;
-  
-  computerTurn = computerSelection();
-  playerTurn = playerSelection()
-  
-  console.log(computerTurn);
-  console.log(playerTurn);
-
-
-  //LOGIC
-  if (computerTurn === playerTurn) {
-    console.log("It's a tie!");
-  }else if (
-    (computerTurn === 'paper' && playerTurn === 'rock') || 
-    (computerTurn === 'scissors' && playerTurn === 'paper') || 
-    (computerTurn === 'rock' && playerTurn === 'scissors')) 
-  {
+  if(playerSelection === computerSelection) {
     computerScore += 10;
-    return ("Game Over\nComputer's score: " +  computerScore  + "\nPlayer's score: " +  playerScore + " \nYou Lose!"); 
-  }else if (
-    (computerTurn === 'scissors' && playerTurn === 'rock') ||
-    (computerTurn === 'rock' && playerTurn === 'paper') || 
-    (computerTurn === 'paper' && playerTurn === 'scissors'))
-  {
     playerScore += 10;
-    return ("Game Over\nComputer's score: " + computerScore + "\nPlayer's score: " + playerScore +" \nYou win!");    
   }
-
+  
+  return winner();
 }
-console.log(playRound(computerPlay, playerPlay));  //Should output a string of who won and who lost.
+
+
+let resultbtn = document.createElement('button');
+function winner() {
+  if(playerScore > computerScore) {
+    document.getElementById('player').textContent = `${playerScore} points!`;
+    document.getElementById('comp').textContent = `${computerScore} points!`;
+    result.appendChild(resultbtn);
+    return resultbtn.innerHTML = `You win &#128514;!`;
+  }else if(playerScore === computerScore){
+    document.getElementById('comp').textContent = `${computerScore} points!`;
+    document.getElementById('player').textContent = `${playerScore} points!`;
+    result.appendChild(resultbtn);
+    return resultbtn.innerHTML = "It's a tie!";
+  }else {
+    document.getElementById('comp').textContent = `${computerScore} points!`;
+    document.getElementById('player').textContent = `${playerScore} points!`;
+    result.appendChild(resultbtn);
+    return resultbtn.innerHTML = `You lose &#128546;!`;
+    
+  }
+  
+}
+
+
+function refreshGame() {
+  location.reload();
+}
+
+
+
+
+//Add EventListeners
+rockbtn.addEventListener('click', playRound);
+scissorsbtn.addEventListener('click', playRound);
+paperbtn.addEventListener('click', playRound);
+refreshBtn.addEventListener('click', refreshGame);
